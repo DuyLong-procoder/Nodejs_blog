f0b802806e10f67efbd78a8117580b8cb1e4346e
DuyLong-procoder <wincute4321@gmail.com>
2025-12-17 03:19:18 +0700

    Add vercel config

--- src/index.js
+++ src/index.js
@@ -20,7 +20,8 @@ app.use(morgan("combined"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(methodOverride("_method"));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "..", "public")));
+
 
 // session
 app.use(
